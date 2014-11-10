8cd5675e2d8df8704d7f05738b7e399dbb84fe91
fgken <fg.bell.tree.pub@gmail.com>
2014-11-10 11:48:04 +0900

    Add Linker Script

--- compile-c.sh
+++ compile-c.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-gcc -S -m32 -masm=intel -fno-asynchronous-unwind-tables -fno-builtin -fno-common -nostdinc -nostdlib -static $1
-./gas2nasm.sed ${1%.*}.s > ${1%.*}.nasm
-nasm ${1%.*}.nasm -o ${1%.*}.bin
+#gcc -S -m32 -masm=intel -fno-asynchronous-unwind-tables -fno-builtin -fno-common -nostdinc -nostdlib -static $1
+#./gas2nasm.sed ${1%.*}.s > ${1%.*}.nasm
+#nasm ${1%.*}.nasm -o ${1%.*}.bin
+
+gcc -T script.ld -fno-asynchronous-unwind-tables -fno-builtin -fno-common -nostdinc -nostdlib -march=x86-64 $1
 
--- fgkenvm.c
+++ fgkenvm.c
@@ -3,6 +3,10 @@ void main(void)
 {
 	int i;
 
+//	__asm__(".intel_syntax noprefix");
+//	__asm__("mov AX, 0x00");
+//	__asm__("cpuid");
+
 	for(i=0xa0000; i <= 0xaffff - 0xffff/2; i++){
 		*(char *)i = 10;
 	}
